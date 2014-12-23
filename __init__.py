0cf7d4188c8459d7823eb32e8665797d46a7af58
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-12-23 13:22:40 +0800

    update

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ class XmasTree(object):
     def __init__(self, height=20, center=80):
         self.height = height
         self.center = center
+        self._new_tree()
 
     def _ornament(self):
         if randint(0, 99) > 80:
@@ -15,22 +16,26 @@ class XmasTree(object):
             return ' '
 
     def _bough(self, n, last=False):
-        first = '/' + "".join([self._ornament() for m in range(0, n * 2 - 1)]) + '\\'
+        first = '/' + "".join([self._ornament()
+                               for _ in range(0, n * 2 - 1)]) + '\\'
         if last:
             second = '/' + '_' * (n * 2 + 1) + '\\'
         else:
-            second = '/' + "".join([self._ornament() for m in range(0, n * 2 - 1)]) + '\\'
+            second = '/' + "".join([self._ornament()
+                                    for _ in range(0, n * 2 - 1)]) + '\\'
         return [first, second]
 
-    def _tree(self):
-        return (['*'] +
-                sum([self._bough(n) for n in range(1, (self.height - 1) // 2)], []) +
-                self._bough((self.height - 1) // 2, True) +
-                ['| |'])
+    def _new_tree(self):
+        self.tree_list = (['*'] +
+                          sum([self._bough(n)
+                               for n in range(1, (self.height - 1) // 2)], []) +
+                          self._bough((self.height - 1) // 2, True) + ['| |'])
+        self.tree = '\n'.join([s.center(self.center)
+                               for s in self.tree_list])
 
     def show(self):
-        print '\n'.join([s.center(self.center)
-                         for s in self._tree()])
+        print self.tree
+
 
 if __name__ == '__main__':
     tree = XmasTree()
